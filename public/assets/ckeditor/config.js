6e22d823cdac094cbe7220bf62b4e5aa86ce0598
kimberly peng <kimmypeng@gmail.com>
2016-07-02 11:37:30 -0400

    adding assets to the pipeline

---
+++ public/assets/ckeditor/config.js
@@ -0,0 +1,9 @@
+// # in app/assets/javascripts/ckeditor/config.js
+
+CKEDITOR.editorConfig = function (config) {
+
+  config.toolbar_mini = [
+    ["Bold",  "Italic", "Format",  "-", "BulletedList", "NumberedList", "-", "Source"],
+  ];
+  config.toolbar = 'mini';
+}
